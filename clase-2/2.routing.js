1f4471eb0e9efc8c6682198ddab4e17b5385c422
Francisco Riorda <riorda7@gmail.com>
2023-08-06 17:50:11 -0300

    corrección

--- clase-2/2.routing.js
+++ clase-2/2.routing.js
@@ -5,6 +5,32 @@ const dittoJSON = require('./pokemon/ditto.json')
 
 const processRequest = (req, res) => {
   const { method, url } = req
+
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>Not Found 404</h1>')
+      }
+
+    case 'POST':
+      switch (url) {
+        case '/pokemon': {
+          const body = ''
+          break
+        }
+        case '/otro': {
+          const body = ''
+          break
+        }
+      }
+  }
 }
 
 const server = http.createServer(processRequest)
